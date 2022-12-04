4bbec97cec4820951fc8e06be078569e689addb7
Joshua_Muniz <joshua.mun97@gmail.com>
2022-12-04 11:32:11 -0700

    Updated Javascript to begin working buttons functionality

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,12 +1,12 @@
 var startTimer = document.querySelector('#TimerJS');
 var startQuiz = document.getElementById('#beginQuiz');
-var showQuestions = document.getElementsByClassName('.hide');
+var showQuestions = document.querySelector('.hide');
 
 
 function timerMain(){
     var seconds = 5;
     var timer = setInterval(function(){
-        document.getElementById('TimerJS').innerHTML='0'+seconds;
+        document.getElementById('TimerJS').innerHTML= +seconds;
         console.log (seconds);
         if (seconds === 0) {
             console.log ("seconds")
@@ -20,21 +20,25 @@ function timerMain(){
 document.getElementById('beginQuiz').addEventListener('click', timerMain);
 document.getElementById('beginQuiz').addEventListener('click', startGame);
 
+
 function startGame(){
     this.style.display = 'none'; 
     if(this.style.display == 'none') {
         console.log(showQuestions);
-        showQuestions.style.display = 'block' ;
+        showQuestions.classList.remove('hide');
+        showQuestions.classList.add('show')
     }
    
-
-
 }
 
 
 function setNextQuestion(){
-
+var Button= document.querySelector('btn');
+    if (Button==document.querySelector('btn'));
+    console.log('Bet');
 }
+
+document.querySelector('btn').addEventListener('click', setNextQuestion);
 
 function selectAnswers(){
 
