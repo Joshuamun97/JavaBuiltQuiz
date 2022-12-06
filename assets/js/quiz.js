49a32de534c647b508a94bc00f415a117e912ea5
Joshua_Muniz <joshua.mun97@gmail.com>
2022-12-05 22:32:24 -0700

    updated to loop through question array. Added form for name entry in html, styled in css.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,17 +1,45 @@
 var startTimer = document.querySelector('#TimerJS');
 var startQuiz = document.getElementById('#beginQuiz');
 var showQuestions = document.querySelector('.hide');
+var ShowForm = document.querySelector('.hidden')
+var question = [
+    {
+        title: 'what City in Arizona has the largest population?',
+        answers: ['Flagstaff', 'Cottonwood', 'Phoenix', 'Sedona'],
+        correctAnswer: 2
+    },
+    {
+        title: 'what Town in Arizona is home to the famous haunted hotel, The Grand Hotel?',
+        answers: ['Cottonwood', 'Payson', 'Phoenix', 'Jerome'],
+        correctAnswer: 3
+    },
+    {
+        title: 'What Nickname was the state of Arizona given?',
+        answers: ['The Hottest State on Earth', 'The Grand Canyon State', 'Rising Sun State', 'The Vortex State'],
+        correctAnswer: 1
+    },
+    {
+        title: 'Which of the following Seven Natural Wonders of The World is Arizona home to?',
+        answers: ['The Grand Canyon', 'Great Barrier Reef', 'Mount Everest', 'Aurora Borealis'],
+        correctAnswer: 0
+    }];
+var titleDiv = document.getElementById('question');
+var arrindex = 0
+
+titleDiv.textContent = question.title;
 
 
-function timerMain(){
+
+function timerMain() {
     var seconds = 5;
-    var timer = setInterval(function(){
-        document.getElementById('TimerJS').innerHTML= +seconds;
-        console.log (seconds);
+    var timer = setInterval(function () {
+        document.getElementById('TimerJS').innerHTML = +seconds;
         if (seconds === 0) {
-            console.log ("seconds")
-            document.getElementById('TimerJS').innerHTML='Sorry, Time is Up!'
+            document.getElementById('TimerJS').innerHTML = 'Sorry, Time is Up!'
             clearInterval(timer);
+        }
+        if (question[arrindex] === undefined) {
+            document.getElementById('TimerJS').innerHTML = 'Submit your score to see how you did compared to other players!'
         }
         seconds--;
 
@@ -21,25 +49,56 @@ document.getElementById('beginQuiz').addEventListener('click', timerMain);
 document.getElementById('beginQuiz').addEventListener('click', startGame);
 
 
-function startGame(){
-    this.style.display = 'none'; 
-    if(this.style.display == 'none') {
-        console.log(showQuestions);
+function startGame() {
+    this.style.display = 'none';
+    if (this.style.display == 'none') {
         showQuestions.classList.remove('hide');
         showQuestions.classList.add('show')
     }
-   
+    questionView(question)
+
+}
+function questionView() {
+
+    if (question[arrindex] !== undefined) {
+        var titleDiv = document.getElementById('question');
+        titleDiv.textContent = question[arrindex].title;
+        var ans = document.querySelectorAll('.btn');
+        ans.forEach(function (element, index) {
+            element.textContent = question[arrindex].answers[index]
+        })
+    }
 }
 
 
-function setNextQuestion(){
-var Button= document.querySelector('btn');
-    if (Button==document.querySelector('btn'));
-    console.log('Bet');
+
+
+
+
+function setNextQuestion() {
+    arrindex++;
+    questionView();
+    if (question[arrindex] === undefined) {
+        showQuestions.classList.remove('show');
+        showQuestions.classList.add('hide');
+        ShowForm.classList.remove('hidden');
+        ShowForm.classList.add('form');
+    }
+
+}
+function scoreKeeper() {
+    var checkAnswer=0;
+    var answer1=document.querySelector('#btn3').addEventListener('click', setNextQuestion);
+    if (question[0].correctAnswer===){
+        console.log('Correct!')
+    }
+
+    
+
 }
 
-document.querySelector('btn').addEventListener('click', setNextQuestion);
+document.querySelector('#btn1').addEventListener('click', setNextQuestion);
+document.querySelector('#btn2').addEventListener('click', setNextQuestion);
+document.querySelector('#btn3').addEventListener('click', setNextQuestion);
+document.querySelector('#btn4').addEventListener('click', setNextQuestion);
 
-function selectAnswers(){
-
-}
